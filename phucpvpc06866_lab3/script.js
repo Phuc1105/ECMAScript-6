fd3108d57d9393d4913983997640a2a610d56551
Phuc1105 <phucpvpc06866@fpt.edu.vn>
2024-01-18 14:27:12 +0700

    [js]-lab3-bài 3,4

--- phucpvpc06866_lab3/script.js
+++ phucpvpc06866_lab3/script.js
@@ -1,55 +1,56 @@
 // bài 1 
 let multiply = (num1, num2) => num1 * num2;
-console.log(multiply(2,3));
+console.log(multiply(2, 3));
 
-let toCelsius = (fahrenheit) => (5/9) * (fahrenheit - 32);
+let toCelsius = (fahrenheit) => (5 / 9) * (fahrenheit - 32);
 console.log(toCelsius(33));
 
-let padZeros = (num, totalLen) =>{
+let padZeros = (num, totalLen) => {
     let numStr = num.toString();
     let numZeros = totalLen - numStr.length;
-    for (let i = 1; i <= numZeros; i++){
+    for (let i = 1; i <= numZeros; i++) {
         numStr = "0" + numStr;
     }
     return numStr;
 }
-console.log(padZeros(9999,6));
+console.log(padZeros(9999, 6));
 
-let power = (base, exponent) =>{
+let power = (base, exponent) => {
     let result = 1;
-    for (let i = 0; i < exponent; i++){
+    for (let i = 0; i < exponent; i++) {
         result *= exponent;
     }
     return result;
 }
-console.log(power(2,2));
+console.log(power(2, 2));
 
 let greet = (who) => "hello" + who;
 console.log(greet(" Phan Văn Phúc"));
 
 // bài 2
-let sumArray = (arr) =>{
+let sumArray = (arr) => {
     let sum = 0;
     arr.forEach(element => {
         sum += element;
-   });
-     
+    });
+
     return sum;
 }
 let mang = [1, 5, 9, 10];
 console.log(sumArray(mang));
 
 // bài 3 
-const Entity = (name, delay) => {
-    this.name = name;
-    this.delay = delay;
-};
-
-Entity.prototype.greet = () => {
-    setTimeout(() => {
-        console.log("Xin chào, tên tôi là", this.name);
-    }, this.delay);
-};
+class Entity {
+    constructor(name, delay) {
+        this.name = name;
+        this.delay = delay;
+    }
+    greet = () => {
+        setTimeout(() => {
+            console.log('Xin chào, tên tôi là ', this.name);
+        }, this.delay);
+    }
+}
 
 let java = new Entity('Java', 5000);
 let cpp = new Entity('C++', 30);
@@ -58,34 +59,18 @@ java.greet();
 cpp.greet();
 
 
-
-//
-// let minus_v1 = (a,b) => {
-//     let xuly = {
-//         name: a,
-//         age: b
-//     }
-//     return xuly;
-// }
-// console.log(minus_v1("JavaScript 1", 1));
-
-// let minus_v2 = (a,b) => ({name: a, age:b});
-// console.log(minus_v2("JavaScript 2", 2));
-
-// var sum = (num1, num2) => ({name: num1 , age: num2 });
-// console.log(sum("JavaScript 3",3));
-
-// let myConcat = (arr1, arr2) =>{
-//     "use strict";
-//     return [...arr1,...arr2];
-// };
-// console.log(myConcat([1],[2]));
-
-// let dog = {
-//     name: "JavaScript",
-//     age: 3,
-//     sua: function(name){
-//         console.log("Con chó" +" "+ this.name + "Gau gau gau" +name);
-//     }
-// }
-// console.log(dog.sua("Ông thầy tèo"));
+// bài 4
+let convertTemperature = (temperature, unit) => {
+    if (unit === "C") {
+        return (temperature * 9 / 5) + 32;
+    } else if (unit === "F") {
+        return (temperature - 32) * 5 / 9;
+    } else {
+        console.log("Vui lòng nhập 'C' hoặc 'F' bạn nhé!!!!!");
+        return "";
+    }
+};
+let celsius = prompt("Nhập vào temperature bạn nhé");
+let fahrenheit = prompt("Nhập vào fahrenheit bạn nhé")
+let temperature = convertTemperature(celsius, fahrenheit);
+alert(`${celsius}°C là ${temperature}°F`);
