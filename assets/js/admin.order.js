ed63b91380e4325f697cd5b44f3ba68232f79e11
Phuc1105 <phucpvpc06866@fpt.edu.vn>
2024-01-24 21:55:43 +0700

    [html][js]-header,footer

--- assets/js/admin.order.js
+++ assets/js/admin.order.js
@@ -13,7 +13,7 @@ fetch(API_URL_ORDERS + 'orders')
                     <td>${order.customer_phone_number}</td>
                     <td>${order.created_date}</td>
                     <td>${order.status}</td>
-                    <td><button type="button" class="btn btn-primary" onclick='clickOrderDetails(${order.id})'>View Details</button></td>
+                    <td><button type="button" class="btn btn-primary" onclick='clickOrderDetails(${order.id})'>See details</button></td>
                 `;
             orderTableBody.appendChild(row);
         });
---
+++ assets/js/import.header.js
@@ -0,0 +1,3 @@
+import { loadHeader, loadFooter } from './client.header.js';
+loadHeader();
+loadFooter();
