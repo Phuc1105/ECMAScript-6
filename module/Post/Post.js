78acc148fd4224f4edb77157c71b7ce0e9223362
Phuc1105 <phucpvpc06866@fpt.edu.vn>
2024-01-30 16:00:43 +0700

    [js]-const API

--- module/Post/Post.js
+++ module/Post/Post.js
@@ -1,18 +1,14 @@
-import {APICaller} from "../Base/APICaller.js";
+import { APICaller } from "../Base/APICaller.js";
 import API_URL from "../Base/APICaller.js";
 
-export default class Post extends APICaller {
-    constructor() {
-        super(API_URL);
-    }
+const apiCaller = new APICaller(API_URL);
 
+export default class Comment {
     static getAll() {
-        const apiCaller = new Post();
         return apiCaller.get('');
     }
 
     static getOne(id) {
-        const apiCaller = new Post();
         return apiCaller.get(id);
     }
 }
---
+++ script.js
@@ -0,0 +1,13 @@
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+    return string.toLowerCase();
+}
+export {uppercaseString, lowercaseString};
+
+function subtract (x, y){
+    return x - y;
+}
+export default subtract;
