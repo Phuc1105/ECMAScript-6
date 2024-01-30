3d4e719c5f912a82424a91509432d8db36ddc841
Phuc1105 <phucpvpc06866@fpt.edu.vn>
2024-01-30 22:38:14 +0700

    Update Comment.js

--- module/Comment/Comment.js
+++ module/Comment/Comment.js
@@ -1,18 +1,14 @@
 import {APICaller} from "../Base/APICaller.js";
 import API_URL from "../Base/APICaller.js";
 
-export default class Comment extends APICaller {
-    constructor() {
-        super(API_URL);
-    }
+const apiCaller = new APICaller(API_URL);
 
+export default class Comment extends APICaller {
     static getAll() {
-        const apiCaller = new Comment();
         return apiCaller.get('');
     }
 
     static getOne(id) {
-        const apiCaller = new Comment();
         return apiCaller.get(id);
     }
 }
